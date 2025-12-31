c909e9d49d38c9fcd01fba3b5ef1ea42b07572b6
Eugene Mulegi <eugenemulegi6@gmail.com>
2025-12-31 22:09:56 +0300

    Remove unnecessary blank lines in app.js

--- app.js
+++ app.js
@@ -7,21 +7,17 @@ const app = express();
 app.listen(8080);
 
 app.get('/', (req, res) => {
-    
     res.sendFile('/index.html', { root: __dirname });
 });
 
 app.get('/about', (req, res) => {
-    
     res.sendFile('/about.html', { root: __dirname });
 });
 
 app.get('/contact-me', (req, res) => {
-    
     res.sendFile('/contact-me.html', { root: __dirname });
 });
 
 app.use((req, res) => {
-
     res.sendFile('./404.html', { root: __dirname });
 });
